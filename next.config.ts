a3203397502bbbb9ddbf745c4e8228e50976afd1
inducesmile54-ux <inducesmile54@gmail.com>
2026-02-16 13:51:27 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: null 
-  },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
   },
-  webpack: (config, { isServer }) => {
-    return config;
-  },
 };
-module.exports = nextConfig;
