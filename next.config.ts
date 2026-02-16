1c0dac91e70d72876a5549be156d8634ebcf9e41
inducesmile54-ux <inducesmile54@gmail.com>
2026-02-16 13:48:43 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
+  experimental: {
+    turbo: null 
   },
-  
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
   },
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      console.log('Client-side webpack config');
-    }
     return config;
   },
 };
-
 module.exports = nextConfig;
